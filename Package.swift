e11a70b70db5508ab3b49ed28bd0fb5b5445ab63
Kasper Lahti <kasper.lahti@tink.se>
2021-01-15 16:01:35 +0100

    Package.swift checksum updates

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TinkPFMUI",
-            path: "TinkPFMUI.xcframework"
+            url: "https://github.com/tink-ab/tink-pfm-ios/releases/download/0.13.1/TinkPFMUI.xcframework.zip", checksum: "ae6148ac2fdbd612221be290536f8839bc9d50191d2fb802a7eb71be594a6c32"
         ),
         .target(name: "TinkPFMUITarget",
                 dependencies: [
