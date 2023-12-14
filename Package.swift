c454c9613efecc6fdff4db69095a02a685f6a0ac
Vitalii Kuznetsov <vitalii.kuznetsov@tink.com>
2023-12-14 16:52:13 +0100

    Package.swift updates

--- Package.swift
+++ Package.swift
@@ -15,12 +15,12 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "TinkCore", url: "https://github.com/tink-ab/tink-core-ios", .upToNextMajor(from: "2.0.0"))
+        .package(name: "TinkCore", url: "https://github.com/tink-ab/tink-core-ios", .upToNextMajor(from: "2.1.0"))
     ],
     targets: [
         .binaryTarget(
             name: "TinkMoneyManagerUI",
-            url: "https://github.com/tink-ab/tink-money-manager-ios/releases/download/1.0.0/TinkMoneyManagerUI.xcframework.zip", checksum: "4bab8959503f9075177501bc0bf6127ac44ca0ecb7eb28ba513e3a6872b08b76"
+            url: "https://github.com/tink-ab/tink-money-manager-ios/releases/download/1.1.0/TinkMoneyManagerUI.xcframework.zip", checksum: "fb58d583889e31691920c1a11f3ff3c17f155cbc2230239c88d1efa77c4f8443"
         ),
         .target(
             name: "TinkMoneyManagerUITarget",
