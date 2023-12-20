c1b3fb8011f19e33d88c162d6dc5e5dac523cc3e
runner <runner@Mac-1703083758774.local>
2023-12-20 14:51:45 +0000

    Package.swift updates

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TinkMoneyManagerUI",
-            url: "https://github.com/tink-ab/tink-money-manager-ios/releases/download/1.1.0/TinkMoneyManagerUI.xcframework.zip", checksum: "fb58d583889e31691920c1a11f3ff3c17f155cbc2230239c88d1efa77c4f8443"
+            url: "https://github.com/tink-ab/tink-money-manager-ios/releases/download/1.1.0/TinkMoneyManagerUI.xcframework.zip", checksum: "91e190396593ab7c9bf9097e005e9394133fced5f0c67fc8523b489fe5458a7a"
         ),
         .target(
             name: "TinkMoneyManagerUITarget",
