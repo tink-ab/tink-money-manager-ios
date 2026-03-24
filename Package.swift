4a7bc52b97a804de555e2ff513ebb970225b2c3a
Anka <runner@iad20-gt1026-baaa9252-8156-43e4-befb-bb2aeb645aa6-8673C8CF388C.local>
2026-03-24 09:23:12 +0000

    Package.swift updates GENAI=NO

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TinkMoneyManagerUI",
-            url: "https://github.com/tink-ab/tink-money-manager-ios/releases/download/3.1.7/TinkMoneyManagerUI.xcframework.zip", checksum: "6b515292bc184516e6eb38f05bfa43b858169fc61916cd3f6fc231b98c9689e9"
+            url: "https://github.com/tink-ab/tink-money-manager-ios/releases/download/3.1.8/TinkMoneyManagerUI.xcframework.zip", checksum: "b64562ba8a18f3c97c0e7d20551c9adba3c6d8e9fc20c2db5898a3ae04522aa4"
         ),
     ]
 )
